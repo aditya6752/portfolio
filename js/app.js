277119d72dc54f139987226d344fd136809d2b5e
Aditya Mishra <aditya.mishra6752@gmail.com>
2021-05-13 14:41:23 +0530

    Done some changes required

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+    hamberger.addEventListener('click', function() {
+        mobileNav.classList.add('open');
+    });
+
+    hamberger.addEventListener('click', function() {
+        mobileNav.classList.remove('open');
+    });
+});
